26188bd44a0ed6b32a4be7641b2974749a4ff0d5
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2022-11-12 22:12:17 +0100

    Use structured data

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -1,8 +1,8 @@
 defmodule TodoList do
   def new, do: %{}
 
-  def add_entry(todo_list, date, title) do
-    Map.update(todo_list, date, [title], fn titles -> [title | titles] end)
+  def add_entry(todo_list, entry) do
+    Map.update(todo_list, entry.date, [entry], &[entry | &1])
   end
 
   def entries(todo_list, date) do
--- test/todo_list_test.exs
+++ test/todo_list_test.exs
@@ -12,19 +12,23 @@ defmodule TodoListTest do
   test "Adding to empty date and reading" do
     todo_list =
       TodoList.new()
-      |> TodoList.add_entry({2013, 12, 19}, "TODO1")
+      |> TodoList.add_entry(%{date: {2013, 12, 19}, title: "TODO1"})
 
     result_list = TodoList.entries(todo_list, {2013, 12, 19})
-    assert result_list == ["TODO1"]
+    assert result_list == [%{date: {2013, 12, 19}, title: "TODO1"}]
   end
 
   test "Adding to existing date and reading" do
     todo_list =
       TodoList.new()
-      |> TodoList.add_entry({2013, 12, 19}, "TODO1")
-      |> TodoList.add_entry({2013, 12, 19}, "TODO2")
+      |> TodoList.add_entry(%{date: {2013, 12, 19}, title: "TODO1"})
+      |> TodoList.add_entry(%{date: {2013, 12, 19}, title: "TODO2"})
 
     result_list = TodoList.entries(todo_list, {2013, 12, 19})
-    assert result_list == ["TODO2", "TODO1"]
+
+    assert result_list == [
+             %{date: {2013, 12, 19}, title: "TODO2"},
+             %{date: {2013, 12, 19}, title: "TODO1"}
+           ]
   end
 end
