c6955eef8d52c5fc9f00654cae9fe53e70a911d9
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2022-11-12 22:28:00 +0100

    Introduce MultiDict abstraction

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -1,11 +1,11 @@
 defmodule TodoList do
-  def new, do: %{}
+  def new, do: MultiDict.new()
 
   def add_entry(todo_list, entry) do
-    Map.update(todo_list, entry.date, [entry], &[entry | &1])
+    MultiDict.add(todo_list, entry.date, entry)
   end
 
   def entries(todo_list, date) do
-    Map.get(todo_list, date, [])
+    MultiDict.get(todo_list, date)
   end
 end
