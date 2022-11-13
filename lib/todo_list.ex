a1b3a7e73bd716b66b1c1557ddf435d6a0fb2554
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2022-11-13 15:34:28 +0100

    Implement using ids

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -1,11 +1,42 @@
 defmodule TodoList do
-  def new, do: MultiDict.new()
+  defstruct auto_id: 1, entries: %{}
 
-  def add_entry(todo_list, entry) do
-    MultiDict.add(todo_list, entry.date, entry)
+  def new, do: %TodoList{}
+
+  def add_entry(%TodoList{auto_id: auto_id, entries: entries}, entry) do
+    entry = Map.put(entry, :id, auto_id)
+    new_entries = Map.put(entries, auto_id, entry)
+
+    %TodoList{auto_id: auto_id + 1, entries: new_entries}
   end
 
-  def entries(todo_list, date) do
-    MultiDict.get(todo_list, date)
+  def entries(%TodoList{entries: entries}, date) do
+    entries
+    |> Stream.filter(fn {_, entry} ->
+      entry.date == date
+    end)
+    |> Enum.map(fn {_, entry} ->
+      entry
+    end)
+  end
+
+  def update_entry(%TodoList{entries: entries} = todo_list, id, updater_fun) do
+    case Map.fetch(entries, id) do
+      :error ->
+        todo_list
+
+      {:ok, entry} ->
+        new_entry = %{id: ^id} = updater_fun.(entry)
+        new_entries = %{entries | id => new_entry}
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def update_entry(%TodoList{} = todo_list, %{id: id, date: _, title: _} = new_entry) do
+    update_entry(todo_list, id, fn _ -> new_entry end)
+  end
+
+  def delete_entry(%TodoList{entries: entries} = todo_list, id) do
+    %TodoList{todo_list | entries: Map.delete(entries, id)}
   end
 end
--- test/todo_list_test.exs
+++ test/todo_list_test.exs
@@ -9,16 +9,16 @@ defmodule TodoListTest do
     assert result_list == []
   end
 
-  test "Adding to empty date and reading" do
+  test "Adding to empty date and reading succeeds" do
     todo_list =
       TodoList.new()
       |> TodoList.add_entry(%{date: {2013, 12, 19}, title: "TODO1"})
 
     result_list = TodoList.entries(todo_list, {2013, 12, 19})
-    assert result_list == [%{date: {2013, 12, 19}, title: "TODO1"}]
+    assert result_list == [%{id: 1, date: {2013, 12, 19}, title: "TODO1"}]
   end
 
-  test "Adding to existing date and reading" do
+  test "Adding to existing date and reading succeeds" do
     todo_list =
       TodoList.new()
       |> TodoList.add_entry(%{date: {2013, 12, 19}, title: "TODO1"})
@@ -27,8 +27,66 @@ defmodule TodoListTest do
     result_list = TodoList.entries(todo_list, {2013, 12, 19})
 
     assert result_list == [
-             %{date: {2013, 12, 19}, title: "TODO2"},
-             %{date: {2013, 12, 19}, title: "TODO1"}
+             %{id: 1, date: {2013, 12, 19}, title: "TODO1"},
+             %{id: 2, date: {2013, 12, 19}, title: "TODO2"}
            ]
+  end
+
+  test "Updating entry succeeds" do
+    todo_list =
+      TodoList.new()
+      |> TodoList.add_entry(%{date: {2013, 12, 19}, title: "TODO1"})
+
+    todo_list =
+      TodoList.update_entry(todo_list, 1, fn _ ->
+        %{id: 1, date: {2013, 12, 20}, title: "TODO2"}
+      end)
+
+    result_list = TodoList.entries(todo_list, {2013, 12, 20})
+    assert result_list == [%{id: 1, date: {2013, 12, 20}, title: "TODO2"}]
+  end
+
+  test "Updating entry with changed id fails" do
+    todo_list =
+      TodoList.new()
+      |> TodoList.add_entry(%{date: {2013, 12, 19}, title: "TODO1"})
+
+    assert_raise MatchError, fn ->
+      TodoList.update_entry(todo_list, 1, fn _ ->
+        %{id: 2, date: {2013, 12, 20}, title: "TODO2"}
+      end)
+    end
+  end
+
+  test "Updating entry with alternative interface succeeds" do
+    todo_list =
+      TodoList.new()
+      |> TodoList.add_entry(%{date: {2013, 12, 19}, title: "TODO1"})
+
+    todo_list = TodoList.update_entry(todo_list, %{id: 1, date: {2013, 12, 20}, title: "TODO2"})
+
+    result_list = TodoList.entries(todo_list, {2013, 12, 20})
+    assert result_list == [%{id: 1, date: {2013, 12, 20}, title: "TODO2"}]
+  end
+
+  test "Updating entry with alternative interface wiht malformed entry fails" do
+    todo_list =
+      TodoList.new()
+      |> TodoList.add_entry(%{date: {2013, 12, 19}, title: "TODO1"})
+
+    assert_raise FunctionClauseError, fn ->
+      TodoList.update_entry(todo_list, %{id: 1, title: "TODO2"})
+    end
+  end
+
+  test "Deleting entry succeeds" do
+    todo_list =
+      TodoList.new()
+      |> TodoList.add_entry(%{date: {2013, 12, 19}, title: "TODO1"})
+
+    todo_list = TodoList.delete_entry(todo_list, 1)
+
+    result_list = TodoList.entries(todo_list, {2013, 12, 19})
+    assert result_list == []
   end
 end
