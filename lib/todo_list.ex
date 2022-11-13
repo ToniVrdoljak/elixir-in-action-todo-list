701ca32b959b9da63702255d4bd7b3f9c80dbb79
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2022-11-13 16:34:40 +0100

    Iterative updates

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -1,7 +1,9 @@
 defmodule TodoList do
   defstruct auto_id: 1, entries: %{}
 
-  def new, do: %TodoList{}
+  def new(entries \\ []) do
+    Enum.reduce(entries, %TodoList{}, &add_entry(&2, &1))
+  end
 
   def add_entry(%TodoList{auto_id: auto_id, entries: entries}, entry) do
     entry = Map.put(entry, :id, auto_id)
--- test/todo_list_test.exs
+++ test/todo_list_test.exs
@@ -9,6 +9,21 @@ defmodule TodoListTest do
     assert result_list == []
   end
 
+  test "Initializing TodoList with entries" do
+    todo_list =
+      TodoList.new([
+        %{date: {2013, 12, 19}, title: "TODO1"},
+        %{date: {2013, 12, 19}, title: "TODO2"}
+      ])
+
+    result_list = TodoList.entries(todo_list, {2013, 12, 19})
+
+    assert result_list == [
+             %{id: 1, date: {2013, 12, 19}, title: "TODO1"},
+             %{id: 2, date: {2013, 12, 19}, title: "TODO2"}
+           ]
+  end
+
   test "Adding to empty date and reading succeeds" do
     todo_list =
       TodoList.new()
