08a36abf909b00b3e1053a9692dbfbbedf37dba7
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2022-11-14 15:56:17 +0100

    Implement todo chache

---
+++ lib/todo/cache.ex
@@ -0,0 +1,29 @@
+defmodule Todo.Cache do
+  use GenServer
+
+  @impl GenServer
+  def init(_) do
+    {:ok, %{}}
+  end
+
+  @impl GenServer
+  def handle_call({:server_process, todo_list_name}, _, todo_servers) do
+    case Map.fetch(todo_servers, todo_list_name) do
+      :error ->
+        {:ok, todo_server_pid} = Todo.Server.start()
+        new_todo_servers = Map.put(todo_servers, todo_list_name, todo_server_pid)
+        {:reply, todo_server_pid, new_todo_servers}
+
+      {:ok, todo_server_pid} ->
+        {:reply, todo_server_pid, todo_servers}
+    end
+  end
+
+  def start do
+    GenServer.start(Todo.Cache, nil)
+  end
+
+  def server_process(cache_pid, todo_list_name) do
+    GenServer.call(cache_pid, {:server_process, todo_list_name})
+  end
+end
